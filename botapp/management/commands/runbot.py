7a9ecae9b5296712d00d81f47600a2a550049821
Vitalii <vitalsem.1973@gmail.com>
2025-06-23 17:05:20 +0300

    telegram bot initializing

--- botapp/management/commands/runbot.py
+++ botapp/management/commands/runbot.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
 from botapp.telegram_bot import run_bot
+from botapp.telegram_bot import app
 
 class Command(BaseCommand):
     help = "Запуск Telegram бота"
 
-    def handle(self, *args, **kwargs):
+    async def handle(self, *args, **kwargs):
+        await app.initialize()
         run_bot()
